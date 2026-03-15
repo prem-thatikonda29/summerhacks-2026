f9221f92d752bdd8ff6d4bec33a46173b3db977b
Prem Thatikonda <premthatikonda.work@gmail.com>
2026-03-15 19:24:44 +0530

    Remove invalid Lenis options (gestureDirection, smooth, smoothTouch, touchMultiplier)

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -8,10 +8,6 @@ export default function LenisProvider({ children }: { children: ReactNode }) {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
-      touchMultiplier: 2,
     });
 
     function raf(time: number) {
